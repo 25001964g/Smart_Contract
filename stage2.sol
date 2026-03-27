33b418215271bc1df161e2f7497a1c9633a2b35a
25001964g <25001964g@connect.polyu.hk>
2026-03-27 11:48:20 +0800

    GameDesignUpdated

--- stage2.sol
+++ stage2.sol
@@ -4,13 +4,11 @@ pragma solidity ^0.8.20;
 contract Game{
 
     address public owner;
-    address public challenger;
+    address public player;
     uint256 private ownerAns;
     uint256 private playerAns;
-    uint256 private result;
-    string private ownerStatis;
     
-    enum Status { None, Submitted }
+    enum Status { None, Submitted, Winner }
 
     Status private playerStatus;
     Status private ownerStatus;
@@ -20,17 +18,11 @@ contract Game{
         owner = msg.sender;
     }
 
-    function joinGame() external {
-        require(msg.sender != owner, "owner can not join Game");
-        require(challenger == address(0), "there is already a player joined");
-
-        challenger = msg.sender;
-    }
-
-
+//Step 1: Once both game host and player submit the Number, that means they agree to join the game
     function ownerSubmit(uint256 _ownerAns) public returns (uint256) {
         require(msg.sender == owner, "only owner");
-        require(challenger != address(0), "no player joined");
+        require(ownerStatus!= Status.Submitted, "You have already submitted");
+        require(ownerStatus!= Status.Winner, "The game is finish, new submission is not allowed");
 
         ownerAns = _ownerAns;
         ownerStatus = Status.Submitted;
@@ -38,31 +30,60 @@ contract Game{
     }
 
     function playerSubmit(uint256 _playerAns) public returns (uint256) {
-        require(msg.sender == challenger, "only challenger");
-        require(challenger != address(0), "you need to joined");
+        require(msg.sender != owner, "owner can not Submit player Answer");
+        require(playerStatus!= Status.Submitted, "You have already submitted");
+        require(playerStatus!= Status.Winner, "The game is finish, new submission is not allowed");
 
+        player = msg.sender;
         playerAns = _playerAns;
         playerStatus = Status.Submitted;
         return playerAns;
     }
 
+//View Function for Answer
     function playerViewAns() public view returns(uint256){
-        require(msg.sender == challenger, "only player");
+        require(msg.sender == player, "only player");
         return playerAns;
     }
-
-    
 
     function ownerViewAns() public view returns (uint256) {
         require(msg.sender == owner, "only owner");
         return ownerAns;
     }
 
-    function gameResult() public view returns(uint256){
-        require(playerStatus == Status.Submitted, "input the value");
-        require(ownerStatus == Status.Submitted, "input the value");
-        return (playerAns+ownerAns)%6+1;
+//Step 3: View game result to know who is the winner
+    function calculateGameResult() private view returns(uint256){
+        uint256 result = (playerAns+ownerAns)%6+1;
+        return result;
     }
 
+    function gameResult() public view returns(uint256, string memory){
+        require(playerStatus == Status.Submitted, "Player do not submit");
+        require(ownerStatus == Status.Submitted, "Owner do not submit");
+        uint256 result = calculateGameResult();
+        if (result<=3){
+            return (result, "Owner win, please confirm you are the Winner and get reward!");
+        } else {
+            return (result, "Player win, please confirm you are the Winner and get reward!");
+        }
+    }
+
+//Step 4: Confirm Winner and Get Reward
+//Changing state for the winner to get reward more safety
+function confirmWinner() public {
+    uint256 result = calculateGameResult();
+
+    if (result <= 3) {
+            require(msg.sender == owner, "Only owner can confirm");
+            require(ownerStatus == Status.Submitted, "You are not allow to confirm winner at this stage");
+
+            ownerStatus = Status.Winner;
+        } else {
+            require(msg.sender == player, "Only player can confirm");
+            require(playerStatus == Status.Submitted, "You are not allow to confirm winner at this stage");
+
+            playerStatus = Status.Winner;
+        }
+    }
 
 }
