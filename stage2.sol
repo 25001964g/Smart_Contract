5602f41b25f5097efe2722ed513a6982c7e746c2
25001964g <25001964g@connect.polyu.hk>
2026-03-29 21:23:59 +0800

    Basic Game Logic without payable

--- stage2.sol
+++ stage2.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.20;
 contract Game{
 
     address public owner;
-    address public player;
     uint256 private ownerAns;
+    uint256 private ownerPW;
+    bytes32 private ownerHash;
+
+    address public player;
     uint256 private playerAns;
+    uint256 private playerPW;
+    bytes32 private playerHash;
     
-    enum Status { None, Submitted, Winner }
+    enum Status { None, Submitted, Confirmed, Winner }
 
     Status private playerStatus;
     Status private ownerStatus;
@@ -19,39 +24,67 @@ contract Game{
     }
 
 //Step 1: Once both game host and player submit the Number, that means they agree to join the game
-    function hashing(uint _ownerAns) private returns(uint256){
-        bytes32 salt = ethers.randomBytes(32);
+
+    //Hash function for commit and reveal
+    //Limitation: There is not any on-chain randomness. Owner and Player need to be responsible for the security of Password. -> Development: Deploy a frontend in JavaScript and generate off-chain randomness.
+    function ownerHashing(uint256 _ownerAns, uint256 _ownerPW) private pure returns (bytes32){
+        return keccak256(abi.encodePacked(_ownerAns, _ownerPW));
     }
-    
-    function ownerSubmit(uint256 _ownerAns) public returns (uint256) {
+
+    function playerHashing(uint256 _playerAns, uint256 _playerPW) private pure returns (bytes32){
+        return keccak256(abi.encodePacked(_playerAns, _playerPW));
+    }
+
+    function ownerSubmit(uint256 _ownerAns, uint256 _ownerPW) public returns (uint256) {
         require(msg.sender == owner, "only owner");
         require(ownerStatus!= Status.Submitted, "You have already submitted");
         require(ownerStatus!= Status.Winner, "The game is finish, new submission is not allowed");
 
         ownerAns = _ownerAns;
+        ownerPW = _ownerPW;
+        ownerHash = ownerHashing(ownerAns,ownerPW);
         ownerStatus = Status.Submitted;
         return ownerAns;
     }
 
-    function playerSubmit(uint256 _playerAns) public returns (uint256) {
+    function playerSubmit(uint256 _playerAns, uint256 _playerPW) public returns (uint256) {
         require(msg.sender != owner, "owner can not Submit player Answer");
         require(playerStatus!= Status.Submitted, "You have already submitted");
         require(playerStatus!= Status.Winner, "The game is finish, new submission is not allowed");
 
         player = msg.sender;
         playerAns = _playerAns;
+        playerPW = _playerPW;
+        playerHash = playerHashing(playerAns,playerPW);
         playerStatus = Status.Submitted;
         return playerAns;
     }
 
+//Step 2: Reveal to authenticate the identity of the one who submit
+    function ownerReveal(uint256 _ownerPW_Confirm) public returns (bool, string memory){
+        require(msg.sender == owner, "Only owner can submit Password");
+        bytes32 checkOwnerHash = keccak256(abi.encodePacked(ownerAns, _ownerPW_Confirm));
+        require(checkOwnerHash == ownerHash, "The Password is not correct, Please try again.");
+            ownerStatus = Status.Confirmed;
+            return (true, "Confirmed");
+    }
+
+    function playerReveal(uint256 _playerPW_Confirm) public returns (bool, string memory){
+        require(msg.sender == player, "Only player can submit Password");
+        bytes32 checkplayerHash = keccak256(abi.encodePacked(playerAns, _playerPW_Confirm));
+        require(checkplayerHash == playerHash, "The Password is not correct, Please try again.");
+            playerStatus = Status.Confirmed;
+            return (true, "Confirmed");
+    }
+
 //View Function for Answer
     function playerViewAns() public view returns(uint256){
-        require(msg.sender == player, "only player");
+        require(msg.sender == player, "only player can view the answer");
         return playerAns;
     }
 
     function ownerViewAns() public view returns (uint256) {
-        require(msg.sender == owner, "only owner");
+        require(msg.sender == owner, "only owner can view the answer");
         return ownerAns;
     }
 
@@ -62,8 +95,8 @@ contract Game{
     }
 
     function gameResult() public view returns(uint256, string memory){
-        require(playerStatus == Status.Submitted, "Player do not submit");
-        require(ownerStatus == Status.Submitted, "Owner do not submit");
+        require(playerStatus == Status.Confirmed, "Player do not reveal");
+        require(ownerStatus == Status.Confirmed, "Owner do not reveal");
         uint256 result = calculateGameResult();
         if (result<=3){
             return (result, "Owner win, please confirm you are the Winner and get reward!");
@@ -73,8 +106,10 @@ contract Game{
     }
 
 //Step 4: Confirm Winner and Get Reward
-//Changing state for the winner to get reward more safety
+//Changing state for the winner to get reward more safety, and Winner should pay for the gas fee
 function confirmWinner() public {
+    require(playerStatus == Status.Confirmed, "Player do not reveal");
+    require(ownerStatus == Status.Confirmed, "Owner do not reveal");
     uint256 result = calculateGameResult();
 
     if (result <= 3) {
